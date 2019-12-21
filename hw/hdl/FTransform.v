8522fdef4b6321ab886020fe981505e169302ee6
zxctja <zxctja@163.com>
2019-12-21 16:43:38 +0800

    commit

--- hw/hdl/FTransform.v
+++ hw/hdl/FTransform.v
@@ -72,7 +72,7 @@ for(i = 0; i < BLOCK_SIZE; i = i + 1)begin
     assign d2 = src_i[BLOCK_SIZE * i + 2] - ref_i[BLOCK_SIZE * i + 2];
     assign d3 = src_i[BLOCK_SIZE * i + 3] - ref_i[BLOCK_SIZE * i + 3];
 
-    wire signed [9 : 0] a0,a1,a2,a3;//10b
+    wire signed [31 : 0] a0,a1,a2,a3;//10b
     assign a0 = d0 + d3;
     assign a1 = d1 + d2;
     assign a2 = d1 - d2;
@@ -87,23 +87,17 @@ for(i = 0; i < BLOCK_SIZE; i = i + 1)begin
         end
         else begin
             tmp[BLOCK_SIZE * i + 0] <= (a0 + a1) * 8;
-            tmp[BLOCK_SIZE * i + 1] <= (a2 * 'd2217 + a3 * 'd5352 + 'd1812) >> 9;
+            tmp[BLOCK_SIZE * i + 1] <= (a2 * 'd2217 + a3 * 'd5352 + 'd1812) >>> 9;
             tmp[BLOCK_SIZE * i + 2] <= (a0 - a1) * 8;
-            tmp[BLOCK_SIZE * i + 3] <= (a3 * 'd2217 - a2 * 'd5352 + 'd937) >> 9;
+            tmp[BLOCK_SIZE * i + 3] <= (a3 * 'd2217 - a2 * 'd5352 + 'd0937) >>> 9;
         end
     end
     
-    wire signed [14 : 0] b0,b1,b2,b3;//15b
+    wire signed [31 : 0] b0,b1,b2,b3;//15b
     assign b0 = tmp[i + 0] + tmp[i + 12];
     assign b1 = tmp[i + 4] + tmp[i +  8];
     assign b2 = tmp[i + 4] - tmp[i +  8];
     assign b3 = tmp[i + 0] - tmp[i + 12];
-    
-    wire signed [31 : 0] c0,c1,c2,c3;//15b
-    assign c0 = (b0 + b1 + 7) >>> 4;
-    assign c1 = (b2 * 'd2217 + b3 * 'd5352 + 'd12000 >>> 16) + (b3 != 0);
-    assign c2 = (b0 - b1 + 7) >>> 4;
-    assign c3 = (b3 * 'd2217 - b2 * 'd5352 + 'd51000 >>> 16);
     
     always @ (posedge clk or negedge rst_n)begin
         if(!rst_n)begin
@@ -113,14 +107,10 @@ for(i = 0; i < BLOCK_SIZE; i = i + 1)begin
             out_i[i + 12] <= 'd0;
         end
         else begin
-            out_i[i +  0] <= c0;
-            out_i[i +  4] <= c1;
-            out_i[i +  8] <= c2;
-            out_i[i + 12] <= c3;
-            //out_i[i +  0] <= (b0 + b1 + 7) >>> 4;
-            //out_i[i +  4] <= (b2 * 'd2217 + b3 * 'd5352 + 'd12000 >>> 16) + (b3 != 0);
-            //out_i[i +  8] <= (b0 - b1 + 7) >>> 4;
-            //out_i[i + 12] <= (b3 * 'd2217 - b2 * 'd5352 + 'd51000 >>> 16);
+            out_i[i +  0] <= (b0 + b1 + 7) >>> 4;
+            out_i[i +  4] <= (b2 * 'd2217 + b3 * 'd5352 + 'd12000 >>> 16) + (b3 != 0);
+            out_i[i +  8] <= (b0 - b1 + 7) >>> 4;
+            out_i[i + 12] <= (b3 * 'd2217 - b2 * 'd5352 + 'd51000 >>> 16);
             //out_i[i +  4] <= (b2 * c0 + b3 * c1 + c2 >>> 16) + (b3 != 0);
             //out_i[i + 12] <= (b3 * c0 - b2 * c1 + c3 >>> 16);
         end
