d5addfc45369cb72f8f9f8b5de350648a248580c
zxctja <zxctja@163.com>
2020-02-19 16:04:08 +0800

    commit

--- hw/hdl/GetCostLuma.v
+++ hw/hdl/GetCostLuma.v
@@ -28,26 +28,15 @@ module GetCostLuma#(
 ,output reg                                               done
 );
 
-reg [3:0]count;
-reg [15:0]shift;
-
-always @ (posedge clk or negedge rst_n)begin
-    if(~rst_n)begin
-        done  <= 'b0;
-        shift <= 'b0;
-    end
-    else begin
-        shift[0] <= start;
-        shift[15:1] <= shift[14:0];
-        done  <= shift[15];
-    end
-end
+reg [ 3:0]count;
+reg [ 0:0]shift;
+reg [31:0]tmp[8:0];
 
 always @ (posedge clk or negedge rst_n)begin
     if(~rst_n)
         count <= 'b0;
     else
-        if(shift[0] | count != 'b0)
+        if(start | count != 'b0)
             count <= count + 1'b1;
 end
 
@@ -66,30 +55,86 @@ end
 endgenerate
 
 always @ (posedge clk or negedge rst_n)begin
+    if(~rst_n)begin
+        tmp[0] <= 'b0;
+        tmp[1] <= 'b0;
+        tmp[2] <= 'b0;
+        tmp[3] <= 'b0;
+        tmp[4] <= 'b0;
+        tmp[5] <= 'b0;
+        tmp[6] <= 'b0;
+        tmp[7] <= 'b0;
+        tmp[8] <= 'b0;
+    end
+    else begin
+        if(start | count != 'b0)begin
+            tmp[0] <= $signed(tmp_ac[count][ 15:  0]) * $signed(tmp_ac[count][ 15:  0]) +
+                      $signed(tmp_ac[count][ 31: 16]) * $signed(tmp_ac[count][ 31: 16]) +
+                      $signed(tmp[0]);
+
+            tmp[1] <= $signed(tmp_ac[count][ 47: 32]) * $signed(tmp_ac[count][ 47: 32]) +
+                      $signed(tmp_ac[count][ 63: 48]) * $signed(tmp_ac[count][ 63: 48]) +
+                      $signed(tmp[1]);
+
+            tmp[2] <= $signed(tmp_ac[count][ 79: 64]) * $signed(tmp_ac[count][ 79: 64]) +
+                      $signed(tmp_ac[count][ 95: 80]) * $signed(tmp_ac[count][ 95: 80]) +
+                      $signed(tmp[2]);
+
+            tmp[3] <= $signed(tmp_ac[count][111: 96]) * $signed(tmp_ac[count][111: 96]) +
+                      $signed(tmp_ac[count][127:112]) * $signed(tmp_ac[count][127:112]) +
+                      $signed(tmp[3]);
+
+            tmp[4] <= $signed(tmp_ac[count][143:128]) * $signed(tmp_ac[count][143:128]) +
+                      $signed(tmp_ac[count][159:144]) * $signed(tmp_ac[count][159:144]) +
+                      $signed(tmp[4]);
+
+            tmp[5] <= $signed(tmp_ac[count][175:160]) * $signed(tmp_ac[count][175:160]) +
+                      $signed(tmp_ac[count][191:176]) * $signed(tmp_ac[count][191:176]) +
+                      $signed(tmp[5]);
+
+            tmp[6] <= $signed(tmp_ac[count][207:192]) * $signed(tmp_ac[count][207:192]) +
+                      $signed(tmp_ac[count][223:208]) * $signed(tmp_ac[count][223:208]) +
+                      $signed(tmp[6]);
+
+            tmp[7] <= $signed(tmp_ac[count][239:224]) * $signed(tmp_ac[count][239:224]) +
+                      $signed(tmp_ac[count][255:240]) * $signed(tmp_ac[count][255:240]) +
+                      $signed(tmp[7]);
+
+            tmp[8] <= $signed(tmp_dc[count]         ) * $signed(tmp_dc[count]         ) + 
+                      $signed(tmp[8]);
+        end
+        else begin
+            tmp[0] <= 'b0;
+            tmp[1] <= 'b0;
+            tmp[2] <= 'b0;
+            tmp[3] <= 'b0;
+            tmp[4] <= 'b0;
+            tmp[5] <= 'b0;
+            tmp[6] <= 'b0;
+            tmp[7] <= 'b0;
+            tmp[8] <= 'b0;
+        end
+    end
+end
+
+always @ (posedge clk or negedge rst_n)begin
     if(~rst_n)
         sum <= 'b0;
     else
-        if(start)
-            sum <= 'b0;
-        else if(shift[0] | count != 'b0)
-            sum <= $signed(tmp_ac[count][ 15:  0]) * $signed(tmp_ac[count][ 15:  0]) +
-                   $signed(tmp_ac[count][ 31: 16]) * $signed(tmp_ac[count][ 31: 16]) +
-                   $signed(tmp_ac[count][ 47: 32]) * $signed(tmp_ac[count][ 47: 32]) +
-                   $signed(tmp_ac[count][ 63: 48]) * $signed(tmp_ac[count][ 63: 48]) +
-                   $signed(tmp_ac[count][ 79: 64]) * $signed(tmp_ac[count][ 79: 64]) +
-                   $signed(tmp_ac[count][ 95: 80]) * $signed(tmp_ac[count][ 95: 80]) +
-                   $signed(tmp_ac[count][111: 96]) * $signed(tmp_ac[count][111: 96]) +
-                   $signed(tmp_ac[count][127:112]) * $signed(tmp_ac[count][127:112]) +
-                   $signed(tmp_ac[count][143:128]) * $signed(tmp_ac[count][143:128]) +
-                   $signed(tmp_ac[count][159:144]) * $signed(tmp_ac[count][159:144]) +
-                   $signed(tmp_ac[count][175:160]) * $signed(tmp_ac[count][175:160]) +
-                   $signed(tmp_ac[count][191:176]) * $signed(tmp_ac[count][191:176]) +
-                   $signed(tmp_ac[count][207:192]) * $signed(tmp_ac[count][207:192]) +
-                   $signed(tmp_ac[count][223:208]) * $signed(tmp_ac[count][223:208]) +
-                   $signed(tmp_ac[count][239:224]) * $signed(tmp_ac[count][239:224]) +
-                   $signed(tmp_ac[count][255:240]) * $signed(tmp_ac[count][255:240]) +
-                   $signed(tmp_dc[count]         ) * $signed(tmp_dc[count]         ) + 
-                   $signed(sum);
+        if(shift)
+            sum <= tmp[0] + tmp[1] + tmp[2] + tmp[3] + tmp[4] + 
+                   tmp[5] + tmp[6] + tmp[7] + tmp[8];
+end
+
+always @ (posedge clk or negedge rst_n)begin
+    if(~rst_n)begin
+        done  <= 'b0;
+        shift <= 'b0;
+    end
+    else begin
+        shift <= count == 'hf;
+        done  <= shift;
+    end
 end
 
 endmodule
